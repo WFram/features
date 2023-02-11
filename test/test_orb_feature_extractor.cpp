8598b7b9fe92adb45c866791cc08c677ec9ce86c
WFram <earl.freedom.ea@gmail.com>
2023-02-11 22:58:09 +0300

    Add parsing parameters from YAML

--- test/test_orb_feature_extractor.cpp
+++ test/test_orb_feature_extractor.cpp
@@ -27,21 +27,31 @@ class TestORBFeatureExtractor : public ::testing::Test {
 
 TEST_F(TestORBFeatureExtractor, testORBFeatureExtractor) {
   auto feature_extractor_config = config_node_["feature_extractor"];
-  std::map<std::string, std::any> parameters;
+  // TODO: back to the loop
+  std::map<std::string, std::any> feature_extractor_parameters;
   for (auto &[key, value] : feature_extractor_config.as<std::map<std::string, YAML::Node>>()) {
-    parameters[key] = value.as<int>();
+    feature_extractor_parameters[key] = value.as<std::string>();
   }
 
-  const auto number_of_features = std::any_cast<int>(parameters.at("number_of_features"));
-  const auto number_of_pyramid_levels = std::any_cast<int>(parameters.at("number_of_pyramid_levels"));
-  const auto scale_factor = std::any_cast<int>(parameters.at("scale_factor"));
+  int number_of_features{1500};
+  int number_of_pyramid_levels{8};
+  Precision scale_factor{1.2};
+
+  std::function read_parameter = [&](const std::string &parameter_name, void *parameter) {
+    auto str_value = std::any_cast<std::string>(feature_extractor_parameters.at(parameter_name));
+    std::stringstream stream(str_value);
+    stream >> parameter;
+  };
+
+  read_parameter("number_of_features", &number_of_features);
+  std::cout << number_of_features << std::endl;
 
   cv::Mat image = cv::imread(test_file_, cv::IMREAD_GRAYSCALE);
   cv::Mat debug_image;
 
   // TODO: solve casting issues
-  orb_feature_extractor::ORBFeatureExtractor feature_extractor(number_of_features, static_cast<size_t>(number_of_pyramid_levels),
-                                                               static_cast<Precision>(scale_factor));
+  orb_feature_extractor::ORBFeatureExtractor feature_extractor(
+      number_of_features, static_cast<size_t>(number_of_pyramid_levels), static_cast<Precision>(scale_factor));
   orb_feature_extractor::Keypoints keypoints;
   feature_extractor.extract(image, keypoints);
 
