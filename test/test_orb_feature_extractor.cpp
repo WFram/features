94a07b6c78894d5012fe65d8c0a8afb372ead9ae
WFram <earl.freedom.ea@gmail.com>
2023-02-12 21:46:54 +0300

    Refactor parsing function

--- test/test_orb_feature_extractor.cpp
+++ test/test_orb_feature_extractor.cpp
@@ -1,4 +1,3 @@
-#include <any>
 #include <iostream>
 #include <vector>
 
@@ -27,31 +26,30 @@ class TestORBFeatureExtractor : public ::testing::Test {
 
 TEST_F(TestORBFeatureExtractor, testORBFeatureExtractor) {
   auto feature_extractor_config = config_node_["feature_extractor"];
-  // TODO: back to the loop
-  std::map<std::string, std::any> feature_extractor_parameters;
+  std::map<std::string, std::string> feature_extractor_parameters;
   for (auto &[key, value] : feature_extractor_config.as<std::map<std::string, YAML::Node>>()) {
     feature_extractor_parameters[key] = value.as<std::string>();
   }
 
   int number_of_features{1500};
-  int number_of_pyramid_levels{8};
+  size_t number_of_pyramid_levels{8};
   Precision scale_factor{1.2};
 
-  std::function read_parameter = [&](const std::string &parameter_name, void *parameter) {
-    auto str_value = std::any_cast<std::string>(feature_extractor_parameters.at(parameter_name));
+  auto read_parameter = [&]<typename T>(const std::string &parameter_name, T &parameter) {
+    auto str_value = feature_extractor_parameters.at(parameter_name);
     std::stringstream stream(str_value);
     stream >> parameter;
   };
 
-  read_parameter("number_of_features", &number_of_features);
-  std::cout << number_of_features << std::endl;
+  read_parameter("number_of_features", number_of_features);
+  read_parameter("number_of_pyramid_levels", number_of_pyramid_levels);
+  read_parameter("scale_factor", scale_factor);
 
   cv::Mat image = cv::imread(test_file_, cv::IMREAD_GRAYSCALE);
   cv::Mat debug_image;
 
-  // TODO: solve casting issues
-  orb_feature_extractor::ORBFeatureExtractor feature_extractor(
-      number_of_features, static_cast<size_t>(number_of_pyramid_levels), static_cast<Precision>(scale_factor));
+  orb_feature_extractor::ORBFeatureExtractor feature_extractor(number_of_features, number_of_pyramid_levels,
+                                                               scale_factor);
   orb_feature_extractor::Keypoints keypoints;
   feature_extractor.extract(image, keypoints);
 
